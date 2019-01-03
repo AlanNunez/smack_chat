3fa4f4b032c41fb0ccce67444f8e23601d7f4520
Alan Nunez <wilson.garcia@verizon.com>
2019-01-03 10:08:03 -0400

    Initial Commit

---
+++ smack_chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack_chat
+//
+//  Created by Desarrollo on 1/3/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
