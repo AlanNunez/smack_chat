e5f238307c5be3996810978aafa53380dc0ed348
Alan Nunez <wilson.garcia@verizon.com>
2019-01-03 15:49:39 -0400

    Adding gradient

---
+++ smack_chat/View/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  smack_chat
+//
+//  Created by Desarrollo on 1/3/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = .blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottomColor: UIColor = .green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
