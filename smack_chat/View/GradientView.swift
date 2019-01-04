60bb6b15ee3ea5de9d76ac68c41324a2816b0642
Alan Nunez <wilson.garcia@verizon.com>
2019-01-04 09:44:00 -0400

    Updating gradient ending position

--- smack_chat/View/GradientView.swift
+++ smack_chat/View/GradientView.swift
@@ -26,7 +26,7 @@ class GradientView: UIView {
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         gradientLayer.frame = self.bounds
         self.layer.insertSublayer(gradientLayer, at: 0)
     }
