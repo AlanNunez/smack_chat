439b353dcfbe443c0ef6d5092988d6e7b952ed10
Alan Nunez <cocoadev@Alans-MacBook-Pro.local>
2019-01-05 10:55:46 -0400

    Connecting channel VC with Login VC

---
+++ Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack_chat
+//
+//  Created by Alan Nunez on 1/5/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
---
+++ smack_chat/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  smack_chat
+//
+//  Created by Alan Nunez on 1/5/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
