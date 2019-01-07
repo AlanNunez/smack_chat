a9b1de91b00a20c964a24ba6b12c4dfc739807df
Alan Nunez <cocoadev@Alans-MacBook-Pro.local>
2019-01-06 20:12:58 -0400

    Unwinding to main screen

---
+++ smack_chat/Controller/CreateAccountVC.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountVC.swift
+//  smack_chat
+//
+//  Created by Alan Nunez on 1/5/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
