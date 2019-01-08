5dcd3795f705c44f14cb225bf6c0d4acecea759f
Alan Nunez <wilson.garcia@verizon.com>
2019-01-08 15:00:59 -0400

    Login in user w/ raw json

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -10,16 +10,19 @@ import Foundation
 
 typealias CompletionHandler = (_ Sucess: Bool) -> ()
 
-//URL Constants
+// URL Constants
 let BASE_URL = "https://smack-app-host.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
-
+let URL_LOGIN = "\(BASE_URL)account/login"
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
 
-//User defaults
+// User defaults
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
