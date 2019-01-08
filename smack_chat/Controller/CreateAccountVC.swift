4cd49cb8fb30b44914de843ab95f23f7301ecc57
Alan Nunez <wilson.garcia@verizon.com>
2019-01-08 08:33:38 -0400

    Added CreateAccountVC

--- smack_chat/Controller/CreateAccountVC.swift
+++ smack_chat/Controller/CreateAccountVC.swift
@@ -2,29 +2,18 @@
 //  CreateAccountVC.swift
 //  smack_chat
 //
-//  Created by Alan Nunez on 1/5/19.
+//  Created by Desarrollo on 1/8/19.
 //  Copyright © 2019 Dev Core. All rights reserved.
 //
 
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
-    */
-
 }
