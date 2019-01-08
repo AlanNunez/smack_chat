e881e02589e165a00e8113d417997d34f78cb31c
Alan Nunez <wilson.garcia@verizon.com>
2019-01-08 12:24:13 -0400

    Registering user to Service

---
+++ smack_chat/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  smack_chat
+//
+//  Created by Desarrollo on 1/8/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    func setupView() {
+          self.layer.cornerRadius = cornerRadius
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+}
