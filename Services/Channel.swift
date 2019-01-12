a11242a2cda40603b4c6aeca2380774fe82de9ca
Alan Nunez <cocoadev@Alans-MacBook-Pro.local>
2019-01-11 22:44:07 -0400

    Parsed channels from API

---
+++ Services/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  smack_chat
+//
+//  Created by Alan Nunez on 11/01/2019.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
