223d2bec4401dd282f7d239c0b6d747d7be8ca41
Alan Nunez <wilson.garcia@verizon.com>
2019-01-24 07:42:53 -0400

    Fix for nil AuthToken

--- smack_chat/View/ChannelCell.swift
+++ smack_chat/View/ChannelCell.swift
@@ -18,7 +18,7 @@ class ChannelCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         if selected {
-            self.layer.backgroundColor = (UIColor(white: 1, alpha: 0.2).cgColor)
+            self.layer.backgroundColor = (UIColor(white: 1, alpha: 0.2).cgColor)            
         } else {
             self.layer.backgroundColor = UIColor.clear.cgColor
         }
