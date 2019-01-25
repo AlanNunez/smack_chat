be9bd5f1fe681783df9fd0f049e1c92290ac03c6
Alan Nunez <raul.nunez01@gmail.com>
2019-01-25 08:39:52 -0400

    Added timeStamp

--- smack_chat/View/MessageCell.swift
+++ smack_chat/View/MessageCell.swift
@@ -25,5 +25,20 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard let isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        let subs = isoDate[end...]
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: subs.appending("Z"))
+        
+        let newFormmatter = DateFormatter()
+        newFormmatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormmatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 }
